c07b5e8337b20538b59521ef24b4cba1fddf94a1
Leah Humphries <leah.humphries@students.plymouth.ac.uk>
2021-01-26 16:51:23 +0000

    Created PHP project with index and data files
    Created a new PHP project with index.php and data.php files, added HTML to index.php

---
+++ Part2/public/index.php
@@ -0,0 +1,28 @@
+<?php
+/*include_once('header.php');
+*/?>
+
+<body class="bg-info">
+<div class="container-fluid col-md-10 offset-md-1"></div>
+<div class="row"></div>
+<div class="card mt-3 px-2 py-2"></div>
+<h1>Listed Buildings in Plymouth</h1>
+<p>This application displays a list of all buildings in Plymouth.</p>
+</body>
+
+https://plymouth.thedata.place/dataset/76ba8e2c-c5a1-4fb8-87a9-60b7c1530db1/resource/79a496aa-fd96-4ab4-a073-cfbe997f3586/download/pccneighbourhoods.geojson
+
+<!--<form action="/=agreement.php" method="get">
+    <div>
+        <input type="radio" id="agree" name="choice" value="agree">
+        <label for="agree">I Agree</label><br>
+        <input type="radio" id="disagree" name="choice" value="disagree">
+        <label for="disagree">I Disagree</label><br>
+        <input type="submit">
+    </div>
+</form>-->
+
+<?php
+/*include_once('footer.php');*/
+?>
+
