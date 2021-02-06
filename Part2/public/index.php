9e0fd2ef0396b78b203f625c5eaa4b4f45e83079
Leah Humphries <leah.humphries@students.plymouth.ac.uk>
2021-02-06 23:05:50 +0000

    Added bootstrap
    Added bootstrap to index.php file

--- Part2/public/index.php
+++ Part2/public/index.php
@@ -1,28 +1,108 @@
 <?php
-/*include_once('header.php');
-*/?>
-
-<body class="bg-info">
-<div class="container-fluid col-md-10 offset-md-1"></div>
-<div class="row"></div>
-<div class="card mt-3 px-2 py-2"></div>
-<h1>Listed Buildings in Plymouth</h1>
-<p>This application displays a list of all buildings in Plymouth.</p>
-</body>
-
-https://plymouth.thedata.place/dataset/76ba8e2c-c5a1-4fb8-87a9-60b7c1530db1/resource/79a496aa-fd96-4ab4-a073-cfbe997f3586/download/pccneighbourhoods.geojson
-
-<!--<form action="/=agreement.php" method="get">
-    <div>
-        <input type="radio" id="agree" name="choice" value="agree">
-        <label for="agree">I Agree</label><br>
-        <input type="radio" id="disagree" name="choice" value="disagree">
-        <label for="disagree">I Disagree</label><br>
-        <input type="submit">
-    </div>
-</form>-->
-
-<?php
-/*include_once('footer.php');*/
+/*/*include_once('header.php');
+*/
 ?>
 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Bootstrap Example</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    <style>
+        /* Remove the navbar's default margin-bottom and rounded borders */
+        .navbar {
+            margin-bottom: 0;
+            border-radius: 0;
+        }
+
+        /* Set height of the grid so .sidenav can be 100% (adjust as needed) */
+        .row.content {height: 450px}
+
+        /* Set gray background color and 100% height */
+        .sidenav {
+            padding-top: 20px;
+            background-color: #f1f1f1;
+            height: 100%;
+        }
+
+        /* Set black background color, white text and some padding */
+        footer {
+            background-color: #555;
+            color: white;
+            padding: 15px;
+        }
+
+        /* On small screens, set height to 'auto' for sidenav and grid */
+        @media screen and (max-width: 767px) {
+            .sidenav {
+                height: auto;
+                padding: 15px;
+            }
+            .row.content {height:auto;}
+        }
+    </style>
+</head>
+<body>
+
+<nav class="navbar navbar-inverse">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#">Logo</a>
+        </div>
+        <div class="collapse navbar-collapse" id="myNavbar">
+            <ul class="nav navbar-nav">
+                <li class="active"><a href="#">Home</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Projects</a></li>
+                <li><a href="#">Contact</a></li>
+            </ul>
+            <ul class="nav navbar-nav navbar-right">
+                <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
+
+<div class="container-fluid text-center">
+    <div class="row content">
+        <div class="col-sm-2 sidenav">
+            <p><a href="#">Link</a></p>
+            <p><a href="#">Link</a></p>
+            <p><a href="#">Link</a></p>
+        </div>
+        <div class="col-sm-8 text-left">
+            <h1>Welcome</h1>
+            <p>This page displays a list of the neighbourhood boundaries in Plymouth.</p>
+            <hr>
+            <h3>Test</h3>
+            <p>Lorem ipsum...</p>
+        </div>
+        <div class="col-sm-2 sidenav">
+            <div class="well">
+                <p>ADS</p>
+            </div>
+            <div class="well">
+                <p>ADS</p>
+            </div>
+        </div>
+    </div>
+</div>
+
+<footer class="container-fluid text-center">
+    <p>Footer Text</p>
+</footer>
+
+</body>
+</html>
+--><?php
+/*/*include_once('footer.php');*/
+?>
